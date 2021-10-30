a9e4fa6c5047363bd352b8f196bbbfe64f3ab8a2
daihoc10d <phungdai.hoc@gmail.com>
2021-10-30 16:35:35 +0700

    16:35 30/10 update cho LT windown

--- DoAn_QLTV/DoAn_QLTV/FormHelp.Designer.cs
+++ DoAn_QLTV/DoAn_QLTV/FormHelp.Designer.cs
@@ -53,9 +53,9 @@ namespace DoAn_QLTV
             this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(571, 23);
+            this.label1.Size = new System.Drawing.Size(651, 23);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Tiểu luận phần mềm quản lý thư viện môn công nghệ phần mềm:";
+            this.label1.Text = "Đồ án phần mềm quản lý thư viện môn Lập trình trên môi trường Windows:";
             // 
             // label2
             // 
@@ -63,9 +63,9 @@ namespace DoAn_QLTV
             this.label2.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(44, 61);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(151, 19);
+            this.label2.Size = new System.Drawing.Size(188, 19);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Tác giả:  Nhóm 4.";
+            this.label2.Text = "Tác giả:  Nhóm DTHQ.";
             // 
             // label3
             // 
@@ -123,9 +123,9 @@ namespace DoAn_QLTV
             this.label8.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(554, 94);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(148, 19);
+            this.label8.Size = new System.Drawing.Size(115, 19);
             this.label8.TabIndex = 7;
-            this.label8.Text = "Dương Thành Phết.";
+            this.label8.Text = "Bùi Mạnh Toàn";
             // 
             // label9
             // 
